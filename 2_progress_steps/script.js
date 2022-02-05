603cbfa8e9d63ce902e3cf2de24bdfb13e5e54b2
Yashodeep <dhasyashodeep32@gmail.com>
2022-02-05 07:21:01 +0530

    added second project

---
+++ 2_progress_steps/script.js
@@ -0,0 +1,53 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+// get NodeList of all the elements with class circle
+const circles = document.querySelectorAll('.circle');
+
+// index
+let currentActive = 1;
+
+// click event on next button
+next.addEventListener('click', () => {
+    currentActive++;
+    // restrict index upto number of steps(circles): 4
+    if(currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update();
+});
+
+prev.addEventListener('click', () => {
+    currentActive--;
+
+    if(currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
+});
+
+const update = () => {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active');
+        } 
+        else {
+            circle.classList.remove('active');
+        }
+    });    
+
+    const actives = document.querySelectorAll('.active');
+    // for filling the progress line
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+
+    if(currentActive === 1) {
+        prev.disabled = true;
+    }
+    else if(currentActive === circles.length) {
+        next.disabled = true;
+    }
+    else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
